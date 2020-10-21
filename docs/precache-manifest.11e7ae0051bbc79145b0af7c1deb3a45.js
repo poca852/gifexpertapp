54fd93f7c0236bc2815266ac8f1e2269ce79c887
david <cuspoca3d@gmail.com>
2020-10-21 08:08:25 -0600

    docs agregado

---
+++ docs/precache-manifest.11e7ae0051bbc79145b0af7c1deb3a45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0deba844bc161124f6916c950c22c4fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "360cf194b82695f543d3",
+    "url": "/static/css/main.edd403fa.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360cf194b82695f543d3",
+    "url": "/static/js/main.6199a26d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
